fc0f01d9aedb6bb692593a813a7e77f9a9499a74
fank <locke_fank@126.com>
2019-04-22 22:23:26 +0800

    modify version

--- FankMusic/ViewController.swift
+++ FankMusic/ViewController.swift
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    public func sayHello() {
+        print("*** sayHello")
     }
-
-
 }
 
